cdd12009603bcbd4bd7205e4ba6708f21a3e1cde
Miles <596442958@qq.com>
2016-02-20 08:03:02 +0800

    Initial Commit

---
+++ 图片下载/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片下载
+//
+//  Created by Miles on 2/20/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片下载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片下载
+//
+//  Created by Miles on 2/20/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
